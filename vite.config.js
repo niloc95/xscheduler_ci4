18261a6db49755251f5f23cac4ffe36802f9bbde
Nilo Cara <nilo.cara@gmail.com>
2025-09-11 20:42:18 +0200

    fix(vite): set base to / to resolve asset path issues on refresh

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
-  base: './',
+  base: '/',
   publicDir: false,
   build: {
     outDir: 'public/build',
